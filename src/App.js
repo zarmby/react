33efe28beb8b3d4bebff3069c8d7f7b94ddb218f
zarmby <armando_ahrslade@hotmail.com>
2021-05-17 09:22:12 -0700

    se cambio el metodo de navegacion

--- src/App.js
+++ src/App.js
@@ -1,41 +1,37 @@
-import React from 'react'
-import Counter2 from './components/counter/Counter2'
-import NavBar from './components/common/navbar/NavBar'
-import SideBar from './components/common/slidebar/SideBar'
-import Home from './components/Home/Home'
+import React from 'react';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
+import Counter2 from './components/counter/Counter2';
+import NavBar from './components/common/navbar/NavBar';
+import SideBar from './components/common/slidebar/SideBar';
+import Home from './components/Home/Home';
 import './App.css';
 
 class App extends React.Component {
 
-  state = {
-    page: 2
-  }
-
-  handlePage = (pageRecived) => {
-    this.setState({
-      page: pageRecived
-    });
-  }
-
 render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <NavBar handlePage={this.handlePage}/>
-      </header>
+    <Router>
+      <NavBar/>
+      
       <div className = "content">
-        <div>
-          <SideBar className = "SideBar"></SideBar>
-        </div>
-        <div className = "excersises">
-        {this.state.page===1
-        ? <Home/>
-        : <Counter2 max = {20}></Counter2 > }
-        </div>
+        <Switch>
+          <Route path="/home">
+            <Home className = "desc"/>
+          </Route>
+          <Route path="/exercises">
+            <SideBar className = "SideBar"/>
+            <Counter2 className = "excersises" max = {20}/>
+          </Route>
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
 }
--- src/components/common/navbar/NavBar.jsx
+++ src/components/common/navbar/NavBar.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import './styles.css';
 import logo from '../../../assets/img/logo-1.png';
+import { Link } from "react-router-dom";
 
 class NavBar extends React.Component {
 
-  ChangePage(page){
-          const { handlePage } = this.props;
-          handlePage(page);
-    }
 
   render() {
     return (
       <div className = "NavBar">
-        <div className = "root-1"><img src={logo} width = "100%" height = "80px"/></div>
-        <div className = "root-2" onClick={() => this.ChangePage(1)}>Home</div>
-        <div className = "root-3" onClick={() => this.ChangePage(2)}>Excercises</div>
+        <div className = "logo"><img src={logo} width = "100%" height = "80px"/></div>
+        <div className = "homePage">
+          <Link to = "/home" className = "NavLink">Home</Link>
+        </div>
+        <div className = "ExcersisesPage" Link to = "/exercises">
+          <Link to = "/exercises" className = "NavLink">exersices</Link>
+        </div>
       </div>
     );
   }
