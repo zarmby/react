98075165fa6391424ab96d05ef09b19085492e32
zarmby <armando_ahrslade@hotmail.com>
2021-05-21 03:38:00 -0500

    se mejoro el add task

--- src/components/TaskManager/TaskManager.jsx
+++ src/components/TaskManager/TaskManager.jsx
@@ -1,18 +1,24 @@
-import React from "react";
-import './style.css'
+import React, {useState} from "react";
+import './style.css';
 
-class TaskManager extends React.Component {
-  render(){
+const TaskManager = () => {
+
+  const [task, settask] = useState("");
+
+  const AddTask = (event) => {
+    settask(event.target.value);
+    console.log(task);
+  }
+  
     return(
       <div className = "container">
         <div class="input-group ">
           <span class="input-group-text" id="basic-addon1">Nueva Tarea</span>
-          <input type="text" class="form-control" placeholder="Descripcion" aria-label="Username" aria-describedby="basic-addon1"></input>
+          <input type="text" value={task} onChange={AddTask} class="form-control" placeholder="Descripcion" aria-label="Username" aria-describedby="basic-addon1"></input>
           <button type="button" class="btn btn-outline-primary" id = "TM-add">Agregar</button>
         </div>
       </div>
     );
-  }
 }
 
 export default TaskManager;
